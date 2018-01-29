41c299559ecab1c535b678a2886b8eafdf423f62
paubramon <paubramonmora@gmail.com>
2018-01-29 13:25:22 +0100

    Solve prospectors killing each other

--- src/main/java/cat/urv/imas/onthology/InitialGameSettings.java
+++ src/main/java/cat/urv/imas/onthology/InitialGameSettings.java
@@ -409,7 +409,7 @@ public class InitialGameSettings extends GameSettings implements Predicate {
                 this.agentList.get(agent.getType()).add(newCell);
             }
             // check old cell
-            if(oldPathCell.getAgents().get(AgentType.DIGGER).isEmpty()) {
+            if(oldPathCell.getAgents().get(AgentType.DIGGER).isEmpty() && oldPathCell.getAgents().get(AgentType.PROSPECTOR).isEmpty()) {
                 this.agentList.get(agent.getType()).remove(oldCell);
             }
         } else {
